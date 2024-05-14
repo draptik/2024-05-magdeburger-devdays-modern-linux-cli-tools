e195345e4756137b500781f6f74b5d99ce41c115
draptik <draptik@users.noreply.github.com>
2024-05-14 12:55:44 +0000

    Deploying to gh-pages from @ draptik/2023-modern-linux-cli-tools@b509b3c36732f8b46097fc973cd9dee100bb9b72 🚀

---
+++ assets/slidev/4-DAxhB6gn.js
@@ -0,0 +1,11 @@
+import{o as e,c as _,k as i,q as t,s as o,A as l,e as s,ad as n}from"../modules/vue-BGmAbelV.js";import{I as p}from"./default-with-footer-CLj1ugAK.js";import{b as r,F as a}from"../index-rX_Gy171.js";import{p as u,u as c,f as d}from"./context-UQYFzARM.js";import"../modules/shiki-Cj9pzzfL.js";const f=s("h1",null,"Target audience",-1),h=s("ul",null,[s("li",null,"linux desktop CLI users"),s("li",null,"linux admins"),s("li",null,"docker users"),s("li",null,"devops/cloud users"),s("li",null,"…")],-1),m=s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-"},[s("span",{class:"line"},[s("span",null,"  __________________________________________")]),n(`
+`),s("span",{class:"line"},[s("span",null," / This is the year of linux on the desktop \\")]),n(`
+`),s("span",{class:"line"},[s("span",null,"|                                            |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"|          ...Windows10 has WSL ;-)          |")]),n(`
+`),s("span",{class:"line"},[s("span",null," \\                                          /")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  ------------------------------------------")]),n(`
+`),s("span",{class:"line"},[s("span",null,"        \\   ^__^")]),n(`
+`),s("span",{class:"line"},[s("span",null,"         \\  (oo)\\_______")]),n(`
+`),s("span",{class:"line"},[s("span",null,"            (__)\\       )\\/\\")]),n(`
+`),s("span",{class:"line"},[s("span",null,"                ||----w |")]),n(`
+`),s("span",{class:"line"},[s("span",null,"                ||     ||")])])],-1),k={__name:"4",setup(g){return u(a),c(),(x,v)=>(e(),_(p,t(o(l(d)(l(a),3))),{default:i(()=>[f,h,m]),_:1},16))}},C=r(k,[["__file","/@slidev/slides/4.md"]]);export{C as default};
